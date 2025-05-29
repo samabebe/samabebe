91f213b35c75a981e158128d39ce9363d966fc4f
Samuel Abebe <samueltamiratabebe@gmail.com>
2025-05-28 22:52:51 -0400

    Create WN10-AU-000050.ps1

---
+++ STIGS/WN10-AU-000050.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+     Enables auditing of process creation events.
+
+.NOTES
+    Author          : Samuel Abebe
+    LinkedIn        : linkedin.com/in/samuel-abebe-518056281/
+    GitHub          : github.com/samabebe
+    Date Created    : 2025-28-05
+    Last Modified   : 2025-28-05
+    Version         : 1.0
+    STIG-ID         : WN10-AU-000050
+
+.TESTED ON
+    Date(s) Tested  : 2025-28-05
+    Tested By       : Samuel Abebe
+    Systems Tested  : Windows10Pro
+    PowerShell Ver. : 7.6.0
+
+.USAGE
+    PS C:\> .\WN10-AU-000050.ps1
+#>
+auditpol /set /subcategory:"Process Creation" /success:enable
