cb2efc36a1a325cc2e9c984d45c832c632dfe167
Samuel Abebe <samueltamiratabebe@gmail.com>
2025-05-28 22:44:13 -0400

    Create WN10-AU-000565.ps1

---
+++ STIGS/WN10-AU-000565.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+    Enables auditing of failed other logon/logoff events.
+
+.NOTES
+    Author          : Samuel Abebe
+    LinkedIn        : linkedin.com/in/samuel-abebe-518056281/
+    GitHub          : github.com/samabebe
+    Date Created    : 2025-28-05
+    Last Modified   : 2025-28-05
+    Version         : 1.0
+    STIG-ID         : WN10-AU-000565
+
+.TESTED ON
+    Date(s) Tested  : 2025-22-05
+    Tested By       : Samuel Abebe
+    Systems Tested  : Windows10Pro
+    PowerShell Ver. : 7.6.0
+
+.USAGE
+    PS C:\> .\WN10-AU-000565.ps1
+#>
+
+auditpol /set /subcategory:"Other Logon/Logoff Events" /failure:enable
